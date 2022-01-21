0eefe940cf8c103cdf4ff0065e78eaad88d7f802
SamerSiam <ssiam05@gmail.com>
2022-01-21 14:32:12 +0200

    handle for env vars

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
-const uri =
-  "mongodb+srv://admin_1:qF0YyvZNTTZGAWoq@test.99q0a.mongodb.net/Test?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://admin_1:${process.env.PASSWORD}@test.99q0a.mongodb.net/Test?retryWrites=true&w=majority`;
 
 // use the connection from Atlas, need to hide the password for security reasons
 mongoose.connect(uri, {
